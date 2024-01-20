b8b48c7c78432314468ae837781fd989ea4edf78
halexandru11 <77728536+halexandru11@users.noreply.github.com>
2024-01-20 19:15:29 +0200

    update: center plot

--- client/main.py
+++ client/main.py
@@ -1,9 +1,7 @@
 import matplotlib.pyplot as plt
 from pyscript import document
 
-
-def show_plot(title: str):
-    plt.plot([1, 2, 3, 4])
-    plt.ylabel("some numbers")
-    plt.title(title)
-    plt.show()
+plt.plot([1, 5, 3, 4])
+plt.ylabel("other numbers")
+plt.title("some numbers")
+plt.show()
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,7 @@
 import './App.css';
 
 function App() {
-  return (
-    <>
-      <button py-click='show_plot'>Show Plot</button>
-      <p>You have to wait for pyscript to load</p>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
