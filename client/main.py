498dbc1272128a7fb66961067bca19e99c5a989e
halexandru11 <77728536+halexandru11@users.noreply.github.com>
2024-01-20 21:59:46 +0200

    remove pyscript and add dropdown to choose which model to run

--- client/main.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt
-from pyscript import document
-
-plt.plot([1, 5, 3, 4])
-plt.ylabel("other numbers")
-plt.title("some numbers")
-plt.show()
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,41 @@
 import './App.css';
+import { useState } from 'react';
+import { Button } from './components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from './components/ui/dropdown-menu';
 
 function App() {
-  return <></>;
+  const [model, setModel] = useState('Prophet');
+
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant='outline'>{model}</Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className='w-56'>
+          <DropdownMenuLabel>Panel Position</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuRadioGroup value={model} onValueChange={setModel}>
+            <DropdownMenuRadioItem value='LSTM'>LSTM</DropdownMenuRadioItem>
+            <DropdownMenuRadioItem value='Prophet'>
+              Prophet
+            </DropdownMenuRadioItem>
+            <DropdownMenuRadioItem value='Seq2Seq'>
+              Seq2Seq
+            </DropdownMenuRadioItem>
+          </DropdownMenuRadioGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
 }
 
 export default App;
