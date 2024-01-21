942e25c0f5a03ed2faf0f0e2ea054b8f1ca94eb4
halexandru11 <77728536+halexandru11@users.noreply.github.com>
2024-01-21 20:39:23 +0200

    update: prophet is trained and can make predictions on new data (must make some changes on what to plot)

---
+++ server/prophet_train.py
@@ -0,0 +1,120 @@
+from prophet import Prophet
+from prophet.serialize import model_to_json, model_from_json
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+from data_frames import FrameLoader
+
+
+class ProphetPredictor:
+    def __init__(self, df) -> None:
+        self.df = df
+
+    def train(self):
+        models = []
+        for column in df.columns:
+            if column == "Timestamp":
+                continue
+            models.append(self.train_column(column))
+        return models
+
+    def train_column(self, column):
+        model = Prophet()
+        model.fit(
+            self.df[["Timestamp", column]]
+            .rename(columns={"Timestamp": "ds", column: "y"})
+            .dropna()
+        )
+
+        return model
+
+    def warm_start_params(self, model: Prophet):
+        # https://facebook.github.io/prophet/docs/additional_topics.html#updating-fitted-models
+        res = {}
+        for pname in ["k", "m", "sigma_obs"]:
+            if model.mcmc_samples == 0:
+                res[pname] = model.params[pname][0][0]
+            else:
+                res[pname] = np.mean(model.params[pname])
+        for pname in ["delta", "beta"]:
+            if model.mcmc_samples == 0:
+                res[pname] = model.params[pname][0]
+            else:
+                res[pname] = np.mean(model.params[pname], axis=0)
+        return res
+
+    def predict(
+        self,
+        df,
+        trained_models,
+    ) -> list[tuple[Prophet, pd.DataFrame]]:
+        models = []
+        for i, model in enumerate(trained_models):
+            column = df.columns[i + 1]
+            models.append(
+                self.predict_column(
+                    df,
+                    column,
+                    model,
+                )
+            )
+        return models
+
+    def predict_column(self, new_df, column, trained_model, periods=48):
+        model = Prophet()
+        model.fit(
+            new_df[["Timestamp", column]]
+            .rename(columns={"Timestamp": "ds", column: "y"})
+            .dropna(),
+            init=self.warm_start_params(trained_model),
+        )
+
+        future = model.make_future_dataframe(
+            periods=periods, freq="H", include_history=True
+        )
+        forecast = model.predict(future)
+
+        return model, forecast
+
+    def generate_plots(self, df, models):
+        figs: list[plt.Figure] = []
+        for i, (model, forecast) in enumerate(models):
+            column = df.columns[i + 1]
+            plt.figure(figsize=(10, 6))
+            model.plot(forecast, xlabel="Data", ylabel=column)
+            plt.legend()
+            plt.title(f"Prognoza pentru parametrul {column}")
+            figs.append(plt.gcf())
+
+        # save figures
+        for i, fig in enumerate(figs):
+            fig.savefig(f"./images/prophet_{i}.png", bbox_inches="tight")
+        return figs
+
+    def save(self, df, models):
+        for i, (model) in enumerate(models):
+            with open(f"./models/prophet/{df.columns[i + 1]}.json", "w") as f:
+                f.write(model_to_json(model))
+
+    def load(self, df):
+        models = []
+        for i, column in enumerate(df.columns):
+            if column == "Timestamp":
+                continue
+            with open(f"./models/prophet/{column}.json", "r") as f:
+                models.append(model_from_json(f.read()))
+        return models
+
+
+if __name__ == "__main__":
+    df = FrameLoader(FrameLoader.RAW).load()
+    df["Timestamp"] = pd.to_datetime(df["Timestamp"])
+    cuttoff = int(len(df) * 0.8)
+    train_df = df.iloc[:cuttoff]
+    test_df = df.iloc[cuttoff:]
+    predictor = ProphetPredictor(train_df)
+    models = predictor.load(train_df)
+    predictor.save(train_df, models)
+    user_models = predictor.predict(test_df, models)
+    predictor.generate_plots(test_df, user_models)
