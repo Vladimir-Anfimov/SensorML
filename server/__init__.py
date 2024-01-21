fd3cca4abaf3e27cca32fdec0fa78f85251a5c68
Vladimir-Anfimov <vladimir@safehouse.ro>
2024-01-21 14:49:50 +0200

    removed useless univar & some folders

--- server/__init__.py
+++ server/__init__.py
@@ -1,4 +1,3 @@
-from rnn_train import RecurentNeuralNetwork
+from lstm_train import RecurentNeuralNetwork
 from params import NORMALIZED_PARAMS_NAMES, RAW_PARAMS_NAMES
 from data_frames import FrameLoader
-from multivar_rnn_interate import MultiVarRNNIterate
